0f220f3be0987d32595c65619d759efb5804ea1f
azam-amir <azamrazamuhammad7@gmail.com>
2024-09-23 18:58:01 +0500

    some changes

--- src/routes/UnAuthenticatedRoutes.js
+++ src/routes/UnAuthenticatedRoutes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
 import FrontendLayout from "../components/FrontendLayout/FrontendLayout";
+import Home from "../pages/Home";
 import PostDetail from "../pages/PostDetail";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
